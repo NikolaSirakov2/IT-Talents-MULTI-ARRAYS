07e7059451b9dcad3de7c7a8ff2c34e63d11d37b
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-21 19:34:07 +0200

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,15 @@
+let array = [
+    [2, 6, 9],
+    [4, 7, 8],
+    [3, 2, 5]
+]
+
+let multiplication = 1
+
+for (let i = 0; i < array.length; i++){
+    for(let h = 0; h < array[i].length; h++){
+        if(i === h){
+            console.log(array[i][h]);
+        }
+    }
+}
