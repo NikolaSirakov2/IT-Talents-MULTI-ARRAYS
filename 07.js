ec06a0f6d155aecee2bf303037cffa45ff8b2ea1
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-21 19:50:03 +0200

    Update 07.js

--- 07.js
+++ 07.js
@@ -1,15 +1,17 @@
 let array = [
     [2, 6, 9],
     [4, 7, 8],
-    [3, 2, 5]
+    [0, 2, 5]
 ]
 
-let multiplication = 1
+let multiplication = 1;
 
 for (let i = 0; i < array.length; i++){
     for(let h = 0; h < array[i].length; h++){
-        if(i === h){
-            console.log(array[i][h]);
+        if(i > h){
+            multiplication *= array[i][h];
         }
     }
 }
+
+console.log(multiplication);
