fcdb4213382dfac8310d5b457ad0ed2edfb2d22f
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-22 10:57:22 +0200

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,13 @@
+let n = 4;
+let m = 6;
+
+let matrix = "";
+
+for(let k = 0; k < n; k++){
+    for(let g = 0; g < m; g++){
+        matrix += ((m*k) + g + 1) + " ";
+    }
+    matrix += "\n";
+}
+
+console.log(matrix);
