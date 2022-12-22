2906cff953d996ed9efe3184c4e396d6744edbb1
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-22 11:20:06 +0200

    Create 11table.js

---
+++ 11table.js
@@ -0,0 +1,17 @@
+let n = 4;
+let m = 6;
+
+let matrix = [];
+
+for(let k = 0; k < n; k++){
+    let newArr = [];
+
+    for(let g = 0; g < m; g++){
+        newArr.push((m*k) + g + 1);
+    }
+
+    matrix.push(newArr);
+    
+}
+
+console.table(matrix);
