cbc2e00638f65a2984f58ec7ac58209997f37c34
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-23 08:03:20 +0200

    Update 13.js

--- 13.js
+++ 13.js
@@ -5,7 +5,7 @@ let matrix = "";
 
 for(let k = 0; k < n; k++){
     for(let g = 0; g < m; g++){
-        matrix += (k + g + 1) + " ";
+        matrix += (g-1)*3 + (g + 1) + " ";
     }
     matrix += "\n";
 }
